e0ec6fd33850295280ca1ece7318d22be55c7c77
irradev <israeldevcorel@gmail.com>
2026-05-07 03:21:05 -0600

    feat: add BuyMeACoffee component

---
+++ src/app/views/view-about/components/buymeacoffe.ts
@@ -0,0 +1,74 @@
+import { Component, signal } from "@angular/core";
+import { IconCoffe } from "../../../components/icons/icon-coffe";
+import { IconLinkExternal } from "../../../components/icons/icon-link-external";
+import { NgClass } from "@angular/common";
+
+
+@Component({
+    selector: 'buymeacoffe',
+    imports: [IconCoffe, IconLinkExternal, NgClass],
+    template: `
+    <div class=" overflow-hidden transition-all duration-1500 h-20 sm:h-12 flex items-center gap-2 bg-amz-neutral" [ngClass]="{
+        'w-9': !isTooltipActive(),
+        'w-[190px] sm:w-[335px] md:w-[385px]  sm:bg-red-200': isTooltipActive(),
+    }" >
+        
+        <button class="btn btn-sm btn-circle btn-ghost hover:bg-transparent hover:border-none" title="¿Me invitas un café?" (click)="toggleTooltip()">
+            <icon-coffe [size]="31" />
+        </button>
+
+        <div class="flex flex-col sm:flex-row justify-start items-center gap-2 sm:gap-1 relative">
+
+            <p class="min-w-[150px] max-w-[150px] md:min-w-[200px] md:max-w-[200px] text-gray-500 text-xs md:text-base font-medium transition-all duration-1500" [ngClass]="{
+                'opacity-0': !isTooltipActive(),
+                'opacity-100': isTooltipActive(),
+            }">
+                Hecho con código, mapas y mucho café ☕️
+            </p>
+
+         
+                
+                <a
+                    href="https://www.buymeacoffee.com/irradev"
+                    target="_blank"
+                    class="flex items-center justify-center gap-2 w-32 text-black text-xs font-bold ml-2 bg-amz-secondary text-center rounded-full px-3 py-2 text-nowrap transition-all duration-1800 ease-in-out"
+                    [ngClass]="{
+                        'opacity-0 translate-x-10 pointer-events-none': !isShowBuyMeACoffee(),
+                        'opacity-100 translate-x-0': isShowBuyMeACoffee(),
+                    }"
+                >
+                    Buy me a coffee
+
+                    <icon-link-external [size]="16" />
+                </a>
+        </div>
+
+    </div>
+    `
+})
+export class Buymeacoffe {
+    public isTooltipActive = signal<boolean>(false);
+
+    public isShowBuyMeACoffee = signal(false);
+
+    private timeoutId?: ReturnType<typeof setTimeout>;
+
+    public toggleTooltip() {
+        const nextState = !this.isTooltipActive();
+
+        this.isTooltipActive.set(nextState);
+
+        if (nextState) {
+
+            this.timeoutId = setTimeout(() => {
+                this.isShowBuyMeACoffee.set(true);
+            }, 1500);
+
+        } else {
+
+            clearTimeout(this.timeoutId);
+
+            this.isShowBuyMeACoffee.set(false);
+        }
+    }
+}
