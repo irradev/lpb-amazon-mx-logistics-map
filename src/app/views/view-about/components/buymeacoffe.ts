812ae24fdc595f6a851b3bc06f84733b12c3c401
irradev <israeldevcorel@gmail.com>
2026-05-08 01:37:28 -0600

    refactor: restructure BuyMeACoffee component UI to include an inviting text label with animation

--- src/app/views/view-about/components/buymeacoffe.ts
+++ src/app/views/view-about/components/buymeacoffe.ts
@@ -8,40 +8,50 @@ import { NgClass } from "@angular/common";
     selector: 'buymeacoffe',
     imports: [IconCoffe, IconLinkExternal, NgClass],
     template: `
-    <div class=" overflow-hidden transition-all duration-1500 h-20 sm:h-12 flex items-center gap-2 bg-amz-neutral" [ngClass]="{
-        'w-9': !isTooltipActive(),
-        'w-[190px] sm:w-[335px] md:w-[385px]': isTooltipActive(),
-    }" >
-        
-        <button class="btn btn-sm btn-circle btn-ghost hover:bg-transparent hover:border-none text-gray-300" title="¿Me invitas un café?" (click)="toggleTooltip()">
-            <icon-coffe [size]="31" />
-        </button>
-
-        <div class="flex flex-col sm:flex-row justify-start items-center gap-2 sm:gap-1 relative">
-
-            <p class="min-w-[150px] max-w-[150px] md:min-w-[200px] md:max-w-[200px] text-gray-500 text-xs md:text-base font-medium transition-all duration-1500" [ngClass]="{
-                'opacity-0': !isTooltipActive(),
-                'opacity-100': isTooltipActive(),
+    <div class="flex items-center gap-1 sm:gap-2  ">
+        <p class="text-gray-500 text-xs md:text-sm font-medium transition-all delay-700 duration-1500 italic text-center" [ngClass]="{
+            'translate-x-2 opacity-0': !isShowBuyMeACoffee(),
+            'translate-x-0 opacity-100': isShowBuyMeACoffee(),
+        }">
+            ¿Me invitas un café?
+        </p>
+        <div class=" overflow-hidden transition-all duration-1500 h-20 sm:h-12 flex items-center gap-2 bg-amz-neutral relative" [ngClass]="{
+            'w-9': !isTooltipActive(),
+            'w-[190px] sm:w-[335px] md:w-[385px]': isTooltipActive(),
+        }" >
+    
+            <button class="btn btn-sm btn-circle btn-ghost hover:bg-transparent hover:border-none text-gray-400" title="¿Me invitas un café?" (click)="toggleTooltip()" [ngClass]="
+            {
+                'text-gray-400' : !isTooltipActive(),
+                'text-gray-500' : isTooltipActive(),
             }">
-                Hecho con código, mapas y mucho café ☕️
-            </p>
-
-         
-                
-                <a
-                    href="https://www.buymeacoffee.com/irradev"
-                    target="_blank"
-                    class="flex items-center justify-center gap-2 w-32 text-black text-xs font-bold ml-2 bg-amz-secondary text-center rounded-full px-3 py-2 text-nowrap transition-all duration-1800 ease-in-out"
-                    [ngClass]="{
-                        'opacity-0 translate-x-10 pointer-events-none': !isShowBuyMeACoffee(),
-                        'opacity-100 translate-x-0': isShowBuyMeACoffee(),
-                    }"
-                >
-                    Buy me a coffee
-
-                    <icon-link-external [size]="16" />
-                </a>
-        </div>
+                <icon-coffe [size]="31" />
+            </button>
+    
+            <div class="flex flex-col sm:flex-row justify-start items-center gap-2 sm:gap-1 relative">
+    
+                <p class="min-w-[150px] max-w-[150px] md:min-w-[200px] md:max-w-[200px] text-gray-500 text-xs md:text-base font-medium transition-all duration-1500 italic" [ngClass]="{
+                    'opacity-0': !isTooltipActive(),
+                    'opacity-100': isTooltipActive(),
+                }">
+                    Hecho con código, mapas y mucho café ☕️
+                </p>
+    
+                    <a
+                        href="https://www.buymeacoffee.com/irradev"
+                        target="_blank"
+                        class="flex items-center justify-center gap-2 w-32 text-black text-xs font-bold ml-2 bg-amz-secondary text-center rounded-full px-3 py-2 text-nowrap transition-all duration-1800 ease-in-out"
+                        [ngClass]="{
+                            'opacity-0 translate-x-10 pointer-events-none': !isShowBuyMeACoffee(),
+                            'opacity-100 translate-x-0': isShowBuyMeACoffee(),
+                        }"
+                    >
+                        Buy me a coffee
+    
+                        <icon-link-external [size]="16" />
+                    </a>
+            </div>
+    </div>
 
     </div>
     `
